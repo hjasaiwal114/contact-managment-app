fc0fd7091a74208e7bad5e3a562fb4ffaee74042
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-07-31 20:19:37 +0530

    convert html to jsonn

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const errorHandler = require("./middleware/errorHandler");
+
 const express  = requires("express");
 const dotenv = require("dotenv").config();
 
@@ -7,8 +9,8 @@ const app  = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
